36c3943c195acde1d36e5eb0b375c9dc5392691d
tomas f <46322567+TomFern@users.noreply.github.com>
2024-01-05 19:09:49 -0300

    generalize test script

--- api/test_request.py
+++ api/test_request.py
@@ -1,8 +1,11 @@
 import requests
+import os
+import sys
 
-# Replace with the URL you want to send the POST request to
-# e.g. url = 'https://mlops-demo-test-purple-river-469.fly.dev/predict'
-url = 'http://127.0.0.1:8080/predict'
+# To change the test url, set the env variable TEST_URL
+# e.g. export TEST_URL='https://mlops-demo-test-purple-river-469.fly.dev/predict'
+default_url = 'http://127.0.0.1:8080/predict'
+url = os.environ.get('TEST_URL', default_url)
 
 data = {
         'MedInc': 3.5,        # Median income in tens of thousands
@@ -27,3 +30,5 @@ response = requests.post(url, json=data, headers=headers)
 # Check the response
 print(f'Status Code: {response.status_code}')
 print(f'Response Body: {response.text}')
+if response.status_code != 200:
+    sys.exit(1)
